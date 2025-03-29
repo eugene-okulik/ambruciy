8e65841668e015eb8176a3cd66356cf25afcf67f
Andrey Rassokhin <ambruciy@gmail.com>
2025-03-30 00:59:59 +0300

    fix linter

--- homework/andrey_rassokhin/Homework4/task_1.py
+++ homework/andrey_rassokhin/Homework4/task_1.py
@@ -1,7 +1,13 @@
 my_dict = {
     'tuple': (1, 2, 3, 4, 5),
     'list': ['red', 'green', 'blue', 'grey', 'white'],
-    'dict': {'name': 'Иван', 'second_name': 'Иванов', 'age': '35', 'city': 'Mocsow', 'gender': 'man'},
+    'dict': {
+        'name': 'Иван',
+        'second_name': 'Иванов',
+        'age': '35',
+        'city': 'Mocsow',
+        'gender': 'man'
+    },
     'set': {1, 10, 100, 1000, 1000},
  }
 
