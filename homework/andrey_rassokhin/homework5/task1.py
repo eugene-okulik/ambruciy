d740188268f66437db7ccc77fe56f4fdc9421fd5
Andrey Rassokhin <ambruciy@gmail.com>
2025-04-01 15:55:47 +0300

    fix linter task 1

--- homework/andrey_rassokhin/homework5/task1.py
+++ homework/andrey_rassokhin/homework5/task1.py
@@ -1,3 +1,5 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
-print(f' Name {name}, \n Last Name {last_name}, \n City {city}, \n Phone {phone}, \n Country {country}')
+print(f' Name {name}, \n Last Name {last_name}, \n '
+      f'City {city}, \n Phone {phone}, \n '
+      f'Country {country}')
