5c07a8f0afbd532541f6cd807e16a1c34a5d28e0
Andrey Rassokhin <ambruciy@gmail.com>
2025-05-20 20:42:42 +0300

    Updated code with functions

--- homework/andrey_rassokhin/Homework16/task1.py
+++ homework/andrey_rassokhin/Homework16/task1.py
@@ -5,17 +5,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-db = mysql.connect(
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSW'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    database=os.getenv('DB_NAME')
-)
-
-cursor = db.cursor(dictionary=True)
-
-select_all_info_student = '''
+query = '''
 select s.name,s.second_name,
 g.title as group_title, b.title as book_title,
 s2.title as subject_title, l.title as lesson_title,
@@ -32,17 +22,39 @@ on l.id  = m.lesson_id
 join subjets s2
 on s2.id = l.subject_id
 '''
-db.commit()
-cursor.execute(select_all_info_student)
-data = cursor.fetchall()
-db.close()
 
-my_path = os.path.dirname(__file__)
-homework_dir_path = os.path.dirname(os.path.dirname(my_path))
-eu_file_path = os.path.join(homework_dir_path, 'eugene_okulik', 'Lesson_16', 'hw_data', 'data.csv')
 
-with open(eu_file_path, newline='') as csv_file:
-    file_data = csv.DictReader(csv_file)
-    for raw in file_data:
-        if raw not in data:
-            print(f'Такой строки нет в БД: {raw}')
+def work_with_db(query):
+    db = mysql.connect(
+        user=os.getenv('DB_USER'),
+        passwd=os.getenv('DB_PASSW'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME')
+    )
+    cursor = db.cursor(dictionary=True)
+    db.commit()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    db.close()
+    return data
+
+
+def path_from_file(*path):
+    my_path = os.path.dirname(__file__)
+    homework_dir_path = os.path.dirname(os.path.dirname(my_path))
+    eu_file_path = os.path.join(homework_dir_path, *path)
+    return eu_file_path
+
+
+def work_with_file(path, data):
+    with open(path, newline='') as csv_file:
+        file_data = csv.DictReader(csv_file)
+        for raw in file_data:
+            if raw not in data:
+                print(f'Такой строки нет в БД: {raw}')
+
+
+result_query = work_with_db(query)
+path = path_from_file('eugene_okulik', 'Lesson_16', 'hw_data', 'data.csv')
+work_with_file(path, result_query)
