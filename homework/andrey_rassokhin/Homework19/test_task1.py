002124ef5c774e998ff9b468b6b14402b58b7015
Andrey Rassokhin <ambruciy@gmail.com>
2025-06-09 15:17:25 +0300

    Update file with conftest

--- homework/andrey_rassokhin/Homework19/test_task1.py
+++ homework/andrey_rassokhin/Homework19/test_task1.py
@@ -4,49 +4,6 @@ import pytest
 url = 'http://167.172.172.115:52353/object'
 
 
-@pytest.fixture(scope='function')
-def create_and_delete_object():
-    data = {
-        "name": "Джонни Депп",
-        "data": {
-            "Профессия": "Актер",
-            "Место проживания": "Великобритания"
-        }
-    }
-    response = requests.post(url, json=data)
-    id_object = response.json()["id"]
-    yield id_object
-    requests.delete(f'{url}/{id_object}')
-
-
-@pytest.fixture(scope='function')
-def adding_text_for_each():
-    print('before test')
-    yield
-    print('after test')
-
-
-@pytest.fixture(scope='session')
-def adding_text_for_all():
-    print('Start testing')
-    yield
-    print('Testing completed')
-
-
-@pytest.fixture(scope='function')
-def create_object():
-    data = {
-        "name": "Джонни Депп",
-        "data": {
-            "Профессия": "Актер",
-            "Место проживания": "Великобритания"
-        }
-    }
-    response = requests.post(url, json=data)
-    id_object = response.json()["id"]
-    return id_object
-
-
 def test_get_all_object(adding_text_for_each, adding_text_for_all):
     response = requests.get(url)
     assert response.status_code == 200, 'URL not found'
